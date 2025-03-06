ced1accd9797a64af6b88d520d46daad2d36c7ad
Jared Sevilla <jaredgsevilla@gmail.com>
2025-03-05 23:58:19 -0800

    Added dashboard link to header

--- src/app/(dashboard)/_components/Header.tsx
+++ src/app/(dashboard)/_components/Header.tsx
@@ -1,4 +1,5 @@
 import { auth, signIn } from "@/lib/auth";
+import Link from "next/link";
 
 const Header = async () => {
   const session = await auth();
@@ -8,7 +9,9 @@ const Header = async () => {
 
   return (
     <div className="h-14 flex border-b-2 items-center p-4">
-      <span className="font-bold text-xl">Dashboard</span>
+      <Link href="./" className="font-bold text-xl">
+        Dashboard
+      </Link>
       <div className="grow"></div>
       <div>{session?.user?.name}</div>
     </div>
